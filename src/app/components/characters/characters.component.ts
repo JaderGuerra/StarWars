80a571317053dda5432c59c57bd725a774539388
Jader <swilor29@gmail.com>
2020-12-14 17:26:08 -0500

    probando

--- src/app/components/characters/characters.component.ts
+++ src/app/components/characters/characters.component.ts
@@ -19,15 +19,23 @@ export class CharactersComponent implements OnInit {
   filtro_valor = '';
 
   constructor(private service: ApiService, private ar: ActivatedRoute) {
+    /* en la ducumentacion oficial dice:
+    Si bien puede llamar a getHeroes () en el constructor, esa no es la mejor práctica.
+
+Reserve el constructor para una inicialización simple, como conectar los parámetros del constructor a las propiedades. El constructor no debería hacer nada. Ciertamente, no debería llamar a una función que realiza solicitudes HTTP a un servidor remoto como lo haría un servicio de datos real.
+
+En su lugar, llame a getHeroes () dentro del gancho del ciclo de vida ngOnInit y deje que Angular llame a ngOnInit () en el momento apropiado después de construir una instancia de HeroesComponent
+     */
     this.load = true;
     const id = this.ar.snapshot.paramMap.get('id')
 
     this.service.characters(id).subscribe((resp: any) => {
       let promesas = resp.characters
       promesas.map((urls: string) => {
-
         this.service.http.get(urls).subscribe((character: any) => {
+          console.log(character.films);
           this.characters.push(character)
+          // console.log(this.characters);
           this.load = false;
         })
 
